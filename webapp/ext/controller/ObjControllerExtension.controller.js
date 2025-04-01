29cfc87cadaef113a44783ebc05055284e0131b9
ROGERIO RIBEIRO <rogerio.ribeiromacedo@ext.europarl.europa.eu>
2025-04-01 07:30:02 +0000

    commit

--- webapp/ext/controller/ObjControllerExtension.controller.js
+++ webapp/ext/controller/ObjControllerExtension.controller.js
@@ -1,53 +1,57 @@
-sap.ui.define(['sap/ui/core/mvc/ControllerExtension'], function (ControllerExtension) {
-	'use strict';
+sap.ui.define(["sap/ui/core/mvc/ControllerExtension"], function (ControllerExtension) {
+    "use strict";
 
-	return ControllerExtension.extend('com.ep.zistocks.ext.controller.ObjControllerExtension', {
-		// this section allows to extend lifecycle hooks or hooks provided by Fiori elements
-		override: {
-			/**
-             * Called when a controller is instantiated and its View controls (if available) are already created.
-             * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-             * @memberOf com.ep.zistocks.ext.controller.ObjControllerExtension
-             */
-			onInit: function (oEvent) {
+    return ControllerExtension.extend("com.ep.zistocks.ext.controller.ObjControllerExtension", {
+        override: {
+            onInit: function (oEvent) {},
 
-			},
+            onPageReady: function (oEvent) {
 
-			onPageReady: function(oEvent){
+                var oTable = this.base.byId("com.ep.zistocks::zz_pv_stock_zite_plantsObjectPage--fe::table::_assets::LineItem");
+                
+                if (oTable) {
+                    if (oTable.getFilterConditions().StorageLocation) {
+                        oTable.setFilterConditions();
+                        oTable.rebind();
+                    }
+                }
 
-				var oTable = this.base.byId("com.ep.zistocks::zz_pv_stock_zite_plantsObjectPage--fe::table::_assets::LineItem");
-            	if (oTable){
-					if (oTable.getFilterConditions().StorageLocation){
-						
-						oTable.setFilterConditions();
-						oTable.rebind();
-					}
-				}
+                let oChart = this.getView().byId("com.ep.zistocks::zz_pv_stock_zite_materialsObjectPage--fe::CustomSubSection::BulletChart--idMonthVizFrame");
+                if (oChart) {
+                    let oHeaderContent = this.getView().byId("com.ep.zistocks::zz_pv_stock_zite_materialsObjectPage--fe::HeaderContentContainer");
+                    
+                    if (oHeaderContent) {
+                        let sMaterialNo = oHeaderContent.getBindingContext().getProperty("materialNo");
+                        let sDG = oHeaderContent.getBindingContext().getProperty("DG");
+                        
+                        if (sMaterialNo) {
+                            var oFilter = new sap.ui.model.Filter({
+                                filters: [
+                                    new sap.ui.model.Filter("materialNo", sap.ui.model.FilterOperator.EQ, sMaterialNo),
+                                    new sap.ui.model.Filter("DG", sap.ui.model.FilterOperator.EQ, sDG)
+                                ],
+                                and: true
+                            });
+                            
+                            var oSingleFilter = new sap.ui.model.Filter("materialNo", sap.ui.model.FilterOperator.EQ, sMaterialNo);
+                            oChart.getDataset().getBinding("data").filter();
+                            oChart.getDataset().getBinding("data").filter(oFilter);
+                            oChart.getDataset().updateBindings(true);
+                        }
+                    }
+                }
 
-				let oVizFrame = this.getView().byId("com.ep.zistocks::zz_pv_stock_zite_materialsObjectPage--fe::CustomSubSection::BulletChart--idMonthVizFrame");
-				if (oVizFrame){
-					let oHeaderContainer = this.getView().byId("com.ep.zistocks::zz_pv_stock_zite_materialsObjectPage--fe::HeaderContentContainer");
-					if (oHeaderContainer){
-						let sMaterial = oHeaderContainer.getBindingContext().getProperty("materialNo");
-						let sDG = oHeaderContainer.getBindingContext().getProperty("DG");
-						if (sMaterial){
-							var oCombinedFilter = new sap.ui.model.Filter({
-								filters: 
-									[
-										new sap.ui.model.Filter("materialNo", sap.ui.model.FilterOperator.EQ, sMaterial), 
-										new sap.ui.model.Filter("DG", sap.ui.model.FilterOperator.EQ, sDG)
-									],
-								and: true
-							});
-							var oFilterMaterial =new sap.ui.model.Filter("materialNo",sap.ui.model.FilterOperator.EQ,sMaterial);
-							oVizFrame.getDataset().getBinding("data").filter();
-							oVizFrame.getDataset().getBinding("data").filter(oCombinedFilter);
-							oVizFrame.getDataset().updateBindings(true);
-							
-						}
-					}
-				}
-			}
-		}
-	});
+                var oSorter = new sap.ui.model.Sorter({
+                    path: 'Quantity', 
+                    descending: true 
+                });
+
+                let oDonut = this.getView().byId("com.ep.zistocks::zz_pv_stock_zite_plantsObjectPage--fe::CustomSubSection::DonutChart--idDonutChart");
+                if (oDonut){
+                    oDonut.getBinding("segments").sort(oSorter);
+                }
+
+            }
+        }
+    });
 });
