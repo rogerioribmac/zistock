42642693f83f74c9932a53abb771ff24da14237f
ROGERIO RIBEIRO <rogerio.ribeiromacedo@ext.europarl.europa.eu>
2025-04-01 07:33:28 +0000

    commit

--- webapp/ext/fragment/BulletChart.js
+++ webapp/ext/fragment/BulletChart.js
@@ -1,38 +1,40 @@
-sap.ui.define([
-    "sap/m/MessageToast",
-    "sap/ui/core/HTML"
-], function(MessageToast,HTMLControl) {
-    'use strict';
+sap.ui.define(["sap/m/MessageToast", "sap/ui/core/HTML"], function (MessageToast, HTML) {
+    "use strict";
 
     return {
-        onVizFrameRenderComplete: function(oEvent){
-
+        onVizFrameRenderComplete: function (oEvent) {
             let oVizFrame = this.byId("com.ep.zistocks::zz_pv_stock_zite_materialsObjectPage--fe::CustomSubSection::BulletChart--idMonthVizFrame");
-            if (oVizFrame){
-                var chartData = oEvent.getSource().getDataset().getBinding("data").getAllCurrentContexts();
-                var oPopOver = new sap.viz.ui5.controls.Popover({
-                    'customDataControl' : function(data){
-                        var values = data.data.val, divStr = "", idx = values[1].value;
-                        var svg = "<svg width='10px' height='10px'><path d='M-5,-5L5,-5L5,5L-5,5Z' fill='#5cbae6' transform='translate(5,5)'></path></svg>";
-                        divStr = divStr + "<div style = 'margin: 15px 30px 0 10px'>" + svg + "<b style='margin-left:10px'>" + values[0].value + "</b></div>";
-                        divStr = divStr + "<div style = 'margin: 5px 30px 0 30px'>" + values[1].name + "<span style = 'float: right'>" + values[1].value + "</span></div>";
-                        divStr = divStr + "<div style = 'margin: 5px 30px 0 30px'>" + values[3].name + "<span style = 'float: right'>" + values[3].value + "</span></div>";
-                        divStr = divStr + "<div style = 'margin: 5px 30px 0 30px'>" + values[2].name + "<span style = 'float: right'>" + values[2].value + "</span></div>";
-                        divStr = divStr + "<div style = 'margin: 5px 30px 0 30px'>" + "Unavailable<span style = 'float: right'>" + chartData[data.target.dataset.id].getProperty("unavailableStock") + "</span></div>";
-                        divStr = divStr + "<div style = 'margin: 5px 30px 15px 30px'>" + "Commited<span style = 'float: right'>" + chartData[data.target.dataset.id].getProperty("commitedStock") + "</span></div>";
-                        return new HTMLControl({content:divStr});
-
+            
+            if (oVizFrame) {
+                var aContexts = oEvent.getSource().getDataset().getBinding("data").getAllCurrentContexts();
+                var oPopover = new sap.viz.ui5.controls.Popover({
+                    customDataControl: function (oData) {
+                        var aDataValues = oData.data.val;
+                        var sContent = "";
+                        var iTargetId = oData.target.dataset.id;
+                        var oContext = aContexts[iTargetId];
+                        
+                        if (aDataValues) {
+                            var sSvgIcon = "<svg width='10px' height='10px'><path d='M-5,-5L5,-5L5,5L-5,5Z' fill='#5cbae6' transform='translate(5,5)'></path></svg>";
+                            sContent += `<div style='margin: 15px 30px 0 10px'>${sSvgIcon}<b style='margin-left:10px'>${aDataValues[0].value}</b></div>`;
+                            sContent += `<div style='margin: 5px 30px 0 30px'>${aDataValues[1].name}<span style='float: right'>${aDataValues[1].value}</span></div>`;
+                            sContent += `<div style='margin: 5px 30px 0 30px'>${aDataValues[3].name}<span style='float: right'>${aDataValues[3].value}</span></div>`;
+                            sContent += `<div style='margin: 5px 30px 0 30px'>${aDataValues[2].name}<span style='float: right'>${aDataValues[2].value}</span></div>`;
+                            sContent += `<div style='margin: 5px 30px 0 30px'>Unavailable<span style='float: right'>${oContext.getProperty("unavailableStock")}</span></div>`;
+                            sContent += `<div style='margin: 5px 30px 15px 30px'>Commited<span style='float: right'>${oContext.getProperty("commitedStock")}</span></div>`;
+                        }
+                        
+                        return new HTML({ content: sContent });
                     }
                 });
-                oPopOver.connect(oVizFrame.getVizUid());
-                oPopOver.setFormatString(sap.ui.core.format.NumberFormat.getFloatInstance({
+                
+                oPopover.connect(oVizFrame.getVizUid());
+                oPopover.setFormatString(sap.ui.core.format.NumberFormat.getFloatInstance({
                     groupingEnabled: true,
                     decimalSeparator: ",",
                     groupingSeparator: "."
                 }).format);
             }
-
         }
-
     };
 });
--- webapp/ext/fragment/DonutChart.js
+++ webapp/ext/fragment/DonutChart.js
@@ -1,34 +1,32 @@
-sap.ui.define([
-    "sap/m/MessageToast"
-], function(MessageToast) {
-    'use strict';
+sap.ui.define(["sap/m/MessageToast"], function (MessageToast) {
+    "use strict";
 
     return {
-
         onSelectionChanged: function (oEvent) {
+            let aFilters = [];
+            let aSelectedSegments = oEvent.getParameter("selectedSegments");
 
-            let aStorLocation = [];
-            let oSegments = oEvent.getParameter("selectedSegments");
-            for (let i=0; i<oSegments.length; i++){
-                aStorLocation.push({ operator :"EQ", validated: "NotValidated", values: [oSegments[i].getProperty("label").slice(0, 4)]});
+            for (let i = 0; i < aSelectedSegments.length; i++) {
+                aFilters.push({
+                    operator: "EQ",
+                    validated: "NotValidated",
+                    values: [aSelectedSegments[i].getProperty("label").slice(0, 4)]
+                });
             }
 
             var oTable = this.byId("com.ep.zistocks::zz_pv_stock_zite_plantsObjectPage--fe::table::_assets::LineItem");
-            if (oTable){
-                if (aStorLocation.length){
-                    var oFilter = {"StorageLocation": aStorLocation };
-                    oTable.setFilterConditions(oFilter);
+            
+            if (oTable) {
+                if (aFilters.length) {
+                    var oFilterConditions = { StorageLocation: aFilters };
+                    oTable.setFilterConditions(oFilterConditions);
                 } else {
                     oTable.setFilterConditions();
                 }
                 oTable.rebind();
             }
-
-		},
-
-        onSegmentedChange: function(oEvent){
-            debugger;
         }
-        
     };
 });
+
+
