0f0d511534253d1d1e3f3ba648043641b5818916
ROGERIO RIBEIRO <rogerio.ribeiromacedo@ext.europarl.europa.eu>
2025-04-03 09:02:43 +0000

    commit

--- webapp/ext/controller/PlantsPageExtension.controller.js
+++ webapp/ext/controller/PlantsPageExtension.controller.js
@@ -1,30 +1,31 @@
-sap.ui.define(["sap/ui/core/mvc/ControllerExtension"], function (ControllerExtension) {
-    "use strict";
+sap.ui.define(['sap/ui/core/mvc/ControllerExtension'], function (ControllerExtension) {
+	'use strict';
 
-    return ControllerExtension.extend("com.ep.zistocks.ext.controller.ObjControllerExtension", {
-        override: {
-            onInit: function (oEvent) {},
+	return ControllerExtension.extend('com.ep.zistocks.ext.controller.PlantsPageExtension', {
+		// this section allows to extend lifecycle hooks or hooks provided by Fiori elements
+		override: {
+			/**
+             * Called when a controller is instantiated and its View controls (if available) are already created.
+             * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+             * @memberOf com.ep.zistocks.ext.controller.PlantsPageExtension
+             */
+			onInit: function () {
+				// you can access the Fiori elements extensionAPI via this.base.getExtensionAPI
+				var oModel = this.base.getExtensionAPI().getModel();
+			},
 
-            onPageReady: function (oEvent) {
-
-                var oTable = this.base.byId("com.ep.zistocks::zz_pv_stock_zite_plantsObjectPage--fe::table::_assets::LineItem");
-                
-                if (oTable) {
-                    if (oTable.getFilterConditions().StorageLocation) {
-                        oTable.setFilterConditions();
-                        oTable.rebind();
-                    }
-                }
+			onPageReady: function (oEvent) {
 
                 let oChart = this.getView().byId("com.ep.zistocks::zz_pv_stock_zite_materialsObjectPage--fe::CustomSubSection::BulletChart--idMonthVizFrame");
                 if (oChart) {
+
                     let oHeaderContent = this.getView().byId("com.ep.zistocks::zz_pv_stock_zite_materialsObjectPage--fe::HeaderContentContainer");
-                    
                     if (oHeaderContent) {
+
                         let sMaterialNo = oHeaderContent.getBindingContext().getProperty("materialNo");
                         let sDG = oHeaderContent.getBindingContext().getProperty("DG");
-                        
                         if (sMaterialNo) {
+
                             var oFilter = new sap.ui.model.Filter({
                                 filters: [
                                     new sap.ui.model.Filter("materialNo", sap.ui.model.FilterOperator.EQ, sMaterialNo),
@@ -37,16 +38,12 @@ sap.ui.define(["sap/ui/core/mvc/ControllerExtension"], function (ControllerExten
                             oChart.getDataset().getBinding("data").filter();
                             oChart.getDataset().getBinding("data").filter(oFilter);
                             oChart.getDataset().updateBindings(true);
+
                         }
                     }
                 }
 
-                var oSorter = new sap.ui.model.Sorter({
-                    path: 'Quantity', 
-                    descending: true 
-                });
-
             }
-        }
-    });
+		}
+	});
 });
